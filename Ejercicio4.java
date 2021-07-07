460f025d676f08e05cc51b827323e427664204ea
RaulSE98 <56968210+RaulSE98@users.noreply.github.com>
2021-07-07 12:11:58 +0200

    Create Ejercicio4.java

---
+++ Ejercicio4.java
@@ -0,0 +1,25 @@
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int N = 1;
+		
+		System.out.println("valor inicial de N = " + N);
+		
+		N = N + 77;
+		
+		System.out.println("valor una vez incrementado N en 77 = " +N);
+		
+		N = N - 3;
+		
+		System.out.println("valor una vez decrementado N en 3 = " +N);
+		
+		N = N * 2;
+		
+		System.out.println("valor una vez duplicado el valor de N =" +N);
+
+	}
+
+}
